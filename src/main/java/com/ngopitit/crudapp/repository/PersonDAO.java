031ed7058b87ea2899d43a302e95391e2ff18b8b
Titipong Keawkhum <80212418+pond7897@users.noreply.github.com>
2025-03-19 14:59:54 +0700

    Add CRUD methods to PersonDAO interface

--- src/main/java/com/ngopitit/crudapp/repository/PersonDAO.java
+++ src/main/java/com/ngopitit/crudapp/repository/PersonDAO.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface PersonDAO {
     void save(Person person);
+
     void delete(Integer id);
+
     Person get(Integer id);
+
     List<Person> getAll();
+
     void update(Person person);
 }
